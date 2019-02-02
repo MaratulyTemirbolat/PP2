1c987bed1d0ba9dc6319f5ff961e76559a424691
MaratulyTemirbolat <snake2009003@mail.ru>
2019-02-03 00:42:56 +0600

    A half of Exercises

---
+++ week2/Task1/Task1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+	class Program
+	{
+		static void Do()
+		{
+			StreamReader sr = new StreamReader("input'.txt");
+			string s = sr.ReadToEnd();
+			bool Check = true;
+			for(int k = 0; k < s.Length; k++)
+			{
+				if (s[k] != s[s.Length - 1 - k])
+					Check = false;
+			}
+			if (Check == true)
+				Console.Write("Yes");
+			else
+			{
+				Console.Write("No");
+			}
+			sr.Close();
+		}
+		static void Main(string[] args)
+		{
+			Do();
+			Console.ReadKey();
+		}
+	}
+}
---
+++ week2/Task2/Task2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+	class Program
+	{
+		static void IsPrime()
+		{
+			StreamReader sr = new StreamReader("../../input.txt");
+			StreamWriter sw = new StreamWriter("../../output.txt");
+			string s = sr.ReadLine();
+			string[] ar = s.Split();
+			
+			for(int k = 0; k < ar.Length; k++)
+			{
+				int a = int.Parse(ar[k]);
+				bool Check = true;
+				if (a == 1)
+				{
+					Check = false;
+				}
+				for (int j = 2; j < a; j++)
+				{
+					if (a % j == 0)
+					{
+						Check = false;
+					}
+				}
+				if (Check == true)
+				{
+					sw.Write(a + " ");
+				}
+			}
+			sr.Close();
+			sw.Close();
+			
+		}
+		static void Main(string[] args)
+		{
+			IsPrime();
+			Console.ReadKey();
+		}
+	}
+}
