fe07d53c7d689a940afafc87ccb03d7a84249cec
MaratulyTemirbolat <snake2009003@mail.ru>
2019-02-05 11:26:17 +0600

    add Task3

---
+++ week2/Task3/Task3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+	class Program
+	{
+		public static void ShowSpace(int level)
+		{
+			for(int k = 0; k < level; k++)
+			{
+				Console.Write("    ");
+			}
+		}
+		public static void UsingDirc(DirectoryInfo dir,int level)
+		{
+			
+			foreach (FileInfo file in dir.GetFiles())
+			{
+				ShowSpace(level+1);
+				Console.WriteLine(file.Name);
+			}
+				foreach (DirectoryInfo direct in dir.GetDirectories())
+				{
+					ShowSpace(level+2);
+					Console.WriteLine(direct.Name);
+				UsingDirc(direct, level + 3);
+				}
+		}
+		
+		static void Main(string[] args)
+		{
+			DirectoryInfo dir = new DirectoryInfo("C:/Users/ww/Desktop/Other Things");
+			Console.WriteLine(dir.Name);
+			UsingDirc(dir, 0);
+			Console.ReadKey();
+		}
+	}
+}
