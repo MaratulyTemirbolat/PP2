a8e4a51833483b20c074c014fe081c4638632ee3
MaratulyTemirbolat <snake2009003@mail.ru>
2019-02-12 23:10:12 +0600

    rechange problems

--- week1/Task4/Task4/Program.cs
+++ week1/Task4/Task4/Program.cs
@@ -11,14 +11,22 @@ namespace Task4
 		static void Main(string[] args)
 		{
 			int n = int.Parse(Console.ReadLine()); // Created the variable n and ask user write it
+			string[,] arrayTwo = new string[n, n]; // Created 2Darray with dimensions n*n
+			for(int k = 0; k < n; k++)
+			{
+				for(int j = 0; j < n; j++)
+				{
+					if (k >= j)  // Condition which works if k more or equal j
+					{
+						arrayTwo[k, j] = "[*]"; // Write [*] into 2Darray
+					}					
+				}
+			}
 			for (int k = 0; k < n; k++)
 			{
 				for(int j = 0; j < n; j++)
 				{
-					if (k >= j) // Condition which works if k more or equal j
-					{
-						Console.Write("[*]"); // Show the [*]
-					}
+					Console.Write(arrayTwo[k, j]); // Show the elements of the 2Darray
 				}
 				Console.Write("\n"); // use to begin the drow from the new line
 			}
