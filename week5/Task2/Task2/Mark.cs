f2a785e0e8703d1589b949e2d6314aa4d85402ba
MaratulyTemirbolat <snake2009003@mail.ru>
2019-02-27 09:49:24 +0600

    New Task

---
+++ week5/Task2/Task2/Mark.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.IO;
+
+
+namespace Task2
+{
+	public class Mark
+	{
+		
+		public int points;
+		public List<Data> datas;
+		public Data just;
+		
+		public Mark()
+		{
+
+		}
+		public  Mark(int points,Data just)
+		{
+			this.just = just;
+			datas = new List<Data>();
+			this.points = points;
+		}
+		public char GetLetter(char Let)
+		{
+			return Let;
+		}
+		public void Des(Mark Ma)
+		{
+			FileStream fil = new FileStream("FileForSer.xml", FileMode.Open, FileAccess.Read);
+			XmlSerializer xs = new XmlSerializer(typeof(Mark));
+			Ma = xs.Deserialize(fil) as Mark;
+			Console.WriteLine(datas[0].name);
+			fil.Close();
+		}
+	}
+	public class Data
+	{
+		public string name;
+		public int Year;
+		public int growth;
+		public Data() { }
+		public Data(string name,int Year,int growth)
+		{
+			this.name = name;
+			this.Year = Year;
+			this.growth = growth;
+		}
+		public override string ToString()
+		{
+			return "Name: " + name + "\nYear: " + Year;
+		}
+	}
+}
---
+++ week5/Task2/Task2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace Task2
+{
+	
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Data LOL = new Data();
+			LOL.name = "Hello";
+			LOL.growth = 170;
+			LOL.Year = 15;
+			Mark m = new Mark(95,LOL);
+			Console.WriteLine(LOL);
+			Data d1 = new Data("Temir", 18, 184);
+			Data d2 = new Data("Assanali", 17, 174);
+			m.datas.Add(d1);
+			m.datas.Add(d2);
+			Console.WriteLine(m.GetLetter('A'));
+			FileStream fs = new FileStream("FileForSer.xml", FileMode.Truncate, FileAccess.ReadWrite);
+			XmlSerializer xs = new XmlSerializer(typeof(Mark));
+			xs.Serialize(fs, m);
+			fs.Close();
+			m.Des(m);
+			
+
+			Console.ReadKey();
+		}
+	}
+}
